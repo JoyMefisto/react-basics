2599c79c0f0b3389887235292a00ee284a9b6570
Shanin Artem <mefistoggc@mail.ru>
2017-05-21 17:36:58 +0300

    Change state

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 
 class Checkbox extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
 
         this.state = {
-            checked: false
+            checked: this.props.initiallyChecked
         };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(event) {
+        this.setState({
+            checked: !this.state.checked
+        }, () => console.log(this.state.checked) );
     }
 
     render() {
         return (
-            <button className="checkbox icon">
+            <button className="checkbox icon" onClick={this.handleClick}>
                 <i className="material-icons">
                     { this.state.checked ? "check_box" : "check_box_outline_blank" }
                 </i>
@@ -20,5 +28,9 @@ class Checkbox extends React.Component {
     }
 
 };
+
+Checkbox.propTypes = {
+    initiallyChecked: React.PropTypes.bool.isRequired
+}
 
 export default Checkbox;
